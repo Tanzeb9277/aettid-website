8e4604397024e0a1d3d11458d3be8c770494c344
Tanzeb Choudhury <Tanzeem123456@gmail.com>
2022-05-11 16:24:45 -0400

    Added post functionality

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,26 @@ function App() {
         <Post/>
       </div>
       <div className='info-card'>
-
+          <div className="ft-recipe">
+            <div className="ft-recipe__thumb">
+                <h3>Today's Featured Recipe</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="Strawberry Waffle" />
+            </div>
+            <div className="ft-recipe__content">
+                <header className="content__header">
+                    <div className="row-wrapper">
+                        <h2 className="recipe-title">Strawberry Waffle</h2>
+                        <div className="user-rating"></div>
+                    </div>
+                    <ul className="recipe-details">
+                        <li className="recipe-details-item time"><i className="ion ion-ios-clock-outline"></i><span className="value">20</span><span className="title">Minutes</span></li>
+                        <li className="recipe-details-item ingredients"><i className="ion ion-ios-book-outline"></i><span className="value">5</span><span className="title">Ingredients</span></li>
+                        <li className="recipe-details-item servings"><i className="ion ion-ios-person-outline"></i><span className="value">4-6</span><span className="title">Serving</span></li>
+                    </ul>
+                </header>
+                <p className="description"> There’s no better way to celebrate May being National Strawberry Month than by sharing a sweet treat with your pup!!! Strawberries...</p>
+                <footer className="content__footer"><a href="#">View Recipe</a></footer>
+            </div>
+        </div>
       </div>
       
     </div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import './Post.css'
+import LineIcon from "react-lineicons";
+
 
 class Post extends Component{
     constructor(props){
         super(props)
+    }
+
+
+    liked = (e) =>{
+        let like = e.target.getAttribute("data-liked")
+        if(like == "false"){
+            e.target.setAttribute("class", "lni lni-lni lni-heart-filled size-md ")
+            e.target.setAttribute("data-liked", "true")
+            e.target.style.color = '#ec5451'
+        }else{
+            e.target.setAttribute("class", "lni lni-lni lni-heart size-md" )
+            e.target.setAttribute("data-liked", "false")
+            e.target.style.color = 'black'
+        }
+        
+    }
+
+    bookmark = (e) =>{
+        let bookmark = e.target.getAttribute("data-bookmark")
+        if(bookmark == "false"){
+            e.target.setAttribute("class", "lni lni-lni lni-bookmark-filled size-md ")
+            e.target.setAttribute("data-bookmark", "true")
+        }else{
+            e.target.setAttribute("class", "lni lni-lni lni-bookmark size-md" )
+            e.target.setAttribute("data-bookmark", "false")
+        }
+        
     }
 
     render(){
@@ -11,13 +40,10 @@ class Post extends Component{
             <div className="post">
                 <div className='sidebar'>
                     <div className='like'>
-
-                    </div>
-                    <div className='save'>
-
+                        <LineIcon name="lni lni-heart" onClick={this.liked} data-liked="false"/>
                     </div>
                     <div className='share'>
-
+                        <LineIcon name="lni lni-share-alt-1" />
                     </div>
                 </div>
                 <div className='main'>
@@ -31,7 +57,7 @@ class Post extends Component{
                         Here is a Title
                     </div>
                     <div className='post-img'>
-                        <img src=''/>
+                        <img src='https://preview.redd.it/jiprd26zvty81.jpg?width=640&crop=smart&auto=webp&s=f2e1b2194ae4d08e1576b54eab04e0fa5327b2fd'/>
                     </div>
                 </div>
             </div>
